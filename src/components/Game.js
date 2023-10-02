dca82318a865c3de32f1fcc14837dafb2b7792c5
ersinev <ersinevci@hotmail.com>
2023-10-02 16:15:09 +0200

    updated

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from "react";
-import Card from "./Card";
-import { cardsData } from "../cards";
-import Timer from "./Timer";
-import PlayerInput from "./PlayerInput";
-import { Button, Col, Container } from "react-bootstrap";
-import Players from "./Players";
-
-
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import { cardsData } from '../cards';
+import Timer from './Timer';
+import PlayerInput from './PlayerInput';
+import { Button, Container } from 'react-bootstrap';
+import Players from './Players';
 
 function Game() {
   const [cardsState, setCardsState] = useState(cardsData);
   const [selectedCards, setSelectedCards] = useState([]);
   const [matchedPairs, setMatchedPairs] = useState([]);
   const [disableClick, setDisableClick] = useState(false);
-  const [showEntryPage, setShowEntryPage] = useState(true); // State to control entry page visibility
-  const [showContainer, setShowContainer] = useState(false); // State to control container visibility
-  const [players, setPlayers] = useState([])
+  const [showEntryPage, setShowEntryPage] = useState(true);
+  const [showContainer, setShowContainer] = useState(false);
+  const [players, setPlayers] = useState([]);
+  const [currentTurn, setCurrentTurn] = useState(null);
+  const [points, setPoints] = useState({});
+  const [closingCards, setClosingCards] = useState(false); // New state for card closing animation
+
   useEffect(() => {
     checkForMatch();
   }, [selectedCards]);
-  
+
+  useEffect(() => {
+    if (players.length > 0) {
+      setCurrentTurn(players[0]);
+    }
+  }, [players]);
 
   const handleClick = (clickedCard) => {
     if (clickedCard.isFlipped || selectedCards.length >= 2 || disableClick) {
-      return; // Ignore clicks on flipped cards, when two cards are already selected, or when waiting for cards to close
+      return;
     }
 
     flipCard(clickedCard.id, true);
@@ -43,54 +50,75 @@ function Game() {
   const checkForMatch = () => {
     if (selectedCards.length === 2) {
       const [firstCard, secondCard] = selectedCards;
-
+  
       if (firstCard.key === secondCard.key) {
-        // Matching pair found, mark them as matched
         setMatchedPairs([...matchedPairs, firstCard.key]);
+  
+        // Award a point to the current player
+        const updatedPoints = { ...points };
+        updatedPoints[currentTurn] = (updatedPoints[currentTurn] || 0) + 1;
+        setPoints(updatedPoints);
+  
+        // Wait for a bit (e.g., 1 second) before proceeding with the next turn
+        setTimeout(() => {
+          // Continue with the next player's turn
+          rotateTurn();
+        }, 1000);
       } else {
-        // Keys don't match, close both cards after a delay
-        setDisableClick(true); // Disable clicks temporarily
+        setDisableClick(true);
+        setClosingCards(true); // Cards are in the process of closing
+  
         setTimeout(() => {
           flipCard(firstCard.id, false);
           flipCard(secondCard.id, false);
-          setDisableClick(false); // Enable clicks after cards are closed
-        }, 1000); // Delay for 1 second
+          setDisableClick(false);
+          setClosingCards(false); // Cards have closed, enable the next player's turn
+  
+          // Continue with the next player's turn
+          rotateTurn();
+        }, 1000);
       }
-
+  
       setSelectedCards([]);
     }
+  };
+
+  const rotateTurn = () => {
+    const currentIndex = players.indexOf(currentTurn);
+    const nextIndex = (currentIndex + 1) % players.length;
+    setCurrentTurn(players[nextIndex]);
   };
 
   const isCardMatched = (card) => {
     return matchedPairs.includes(card.key);
   };
 
-  // Function to handle starting the game and showing the container
   const handleStartGame = () => {
-    setShowEntryPage(false); // Hide the entry page
-    setShowContainer(true); // Show the container
-    setPlayers(["Player1","Player2","Player3",])
+    setShowEntryPage(false);
+    setShowContainer(true);
+    setPlayers(['Player1', 'Player2', 'Player3', 'Player4']);
   };
 
   return (
     <>
-      <Container fluid >
-        
+      <Container fluid>
         {showEntryPage && (
-          <div className="entryPage" style={{ color: "white" }}>
-            <PlayerInput/>
-            
+          <div className='entryPage' style={{ color: 'white' }}>
+            <PlayerInput />
             <Button onClick={handleStartGame}>Start Game</Button>
-            
           </div>
         )}
 
-        {/* Conditionally render the container based on showContainer */}
         {showContainer && (
-          <Container className="container-fluid">
+          <Container className='container-fluid'>
             <Timer />
-            <Players players={players}/>
-            <div className="memory-game">
+            <Players
+              players={players}
+              currentTurn={currentTurn}
+              onPlayerClick={setCurrentTurn}
+              points={points}
+            />
+            <div className='memory-game'>
               {cardsState.map((card) => (
                 <Card
                   key={card.id}
@@ -98,6 +126,7 @@ function Game() {
                   onClick={() => handleClick(card)}
                   image={card.img}
                   isMatched={isCardMatched(card)}
+                  disableClick={disableClick || closingCards} // Disable clicks during card closing animation
                 />
               ))}
             </div>
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function Players({players}) {
-    let mdSize;
-    if (players.length === 2) {
-      mdSize = 6;
-    } else if (players.length === 3) {
-      mdSize = 4;
-    } else if (players.length === 4) {
-      mdSize = 3;
-    } else {
-      // Default to 12 columns if the number of players doesn't match your conditions
-      mdSize = 12;
-    }
+function Players({ players, currentTurn, onPlayerClick, points }) {
+  // Calculate mdSize based on the number of players
+  let mdSize;
+  if (players.length === 2) {
+    mdSize = 6;
+  } else if (players.length === 3) {
+    mdSize = 4;
+  } else if (players.length === 4) {
+    mdSize = 3;
+  } else {
+    // Default to 12 columns if the number of players doesn't match your conditions
+    mdSize = 12;
+  }
+
   return (
-    <>
     <Container fluid className='players'>
-        <Row>
+      <Row>
         {players.map((player, index) => (
-          <Col key={index} md={mdSize}>
-            {player}
+          <Col
+            key={index}
+            md={mdSize} // Use mdSize to set the column size
+            style={{
+              backgroundColor: currentTurn === player ? 'green' : 'transparent',
+              padding: '10px',
+            }}
+            onClick={() => onPlayerClick(player)}
+          >
+            {`${player}: ${points[player] || 0}`}
           </Col>
         ))}
-        </Row>
-        
+      </Row>
     </Container>
-  
-  </>
-  )
+  );
 }
 
-export default Players
+export default Players;
