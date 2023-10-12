2676ebde9fc15910a86d7a1d68993ddad6b7681d
ersinev <ersinevci@hotmail.com>
2023-10-13 00:24:56 +0200

    updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,5 @@
-
-
-function Card({ card, onClick,image }) {
-  
+function Card({ card, onClick, image }) {
+  console.log(card)
   return (
     <div
       className={`memory-card${card.isFlipped ? " flip" : ""}`}
@@ -9,8 +7,12 @@ function Card({ card, onClick,image }) {
       style={{ order: card.order }}
       data-testid={card.id}
     >
-      <img className="front-face" src={require(`../assets/images/${image}`)} alt="Card" />
-      <img className="back-face" src={require('./logo.png')} alt="JS Badge" />
+      <img
+        className="front-face"
+        src={require(`../assets/images/${image}`)}
+        alt="Card"
+      />
+      <img className="back-face" src={require("./logo.png")} alt="JS Badge" />
     </div>
   );
 }
