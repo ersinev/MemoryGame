d5631ac10000c303b62903a1f48639dd62f3eaa1
ersinev <ersinevci@hotmail.com>
2024-01-08 11:14:20 +0100

    updated

--- src/components/Players.js
+++ src/components/Players.js
@@ -2,31 +2,20 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 function Players({ players, currentTurn, points }) {
-  let mdSize;
-  if (players.length === 2) {
-    mdSize = 6;
-  } else if (players.length === 3) {
-    mdSize = 4;
-  } else if (players.length === 4) {
-    mdSize = 3;
-  } else {
-    mdSize = 12;
-  }
 
-  
+
+
   return (
     <Container fluid className="players">
-      <Row>
+      <Row style={{ display: "flex", justifyContent: "space-between" }}>
         {players.map((player, index) => (
           <Col
             key={index}
-            md={mdSize}
             style={{
               backgroundColor:
                 currentTurn === player.id ? "green" : "transparent",
               padding: "10px",
             }}
-            
           >
             {`${player.name}: ${points[player.id] || 0}`}
           </Col>
