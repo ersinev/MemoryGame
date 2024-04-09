c22b55ef85aad7c8d07418c8c43f6d104faf6147
ersinev <ersinevci@hotmail.com>
2024-04-09 17:03:05 +0200

    updated

--- src/components/PlayerInput.test.js
+++ src/components/PlayerInput.test.js
@@ -4,11 +4,38 @@ import "@testing-library/jest-dom"; // Updated import statement
 import PlayerInput from "./PlayerInput";
 
 describe('Game component for testing', () => {
-    
+
     test('should render player input page', () => {
-        render(<PlayerInput/>);
-        const startButton = screen.getByText(/Start Game/i);
-        expect(startButton).toBeInTheDocument();  
+        render(<PlayerInput />);
+
     });
+
+    test('allows user to input name and room ID', () => {
+        render(<PlayerInput />);
+        const nameInput = screen.getByLabelText(/Naam/i);
+        const roomIdInput = screen.getByLabelText(/Room ID/i);
+
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(roomIdInput, { target: { value: 'room123' } });
+
+        expect(nameInput.value).toBe('John');
+        expect(roomIdInput.value).toBe('room123');
+    });
+
+    test('calls onJoinGame prop when Start Game button is clicked with valid inputs', () => {
+        const onJoinGameMock = jest.fn();
+        render(<PlayerInput onJoinGame={onJoinGameMock} />);
+        const nameInput = screen.getByLabelText(/Naam/i);
+        const roomIdInput = screen.getByLabelText(/Room ID/i);
+        const startGameButton = screen.getByText(/Start Game/i);
+    
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(roomIdInput, { target: { value: 'room1' } });
+        fireEvent.click(startGameButton);
+    
+        expect(onJoinGameMock).toHaveBeenCalledWith('room1', 'John');
+      });
+
+
 
 });
