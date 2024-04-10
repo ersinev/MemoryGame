d7be66fbee5c9c88795d4bf26e1d9fbdecea307c
ersinev <ersinevci@hotmail.com>
2024-04-10 12:33:57 +0200

    Result/Players tests added

--- src/components/Result.test.js
+++ src/components/Result.test.js
@@ -3,10 +3,7 @@ import Result from "./Result";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom"; 
 
-
-
 describe('Result component', () => {
-
     test('renders winners with their points', () => {
         const players = [
           { id: 1, name: 'Player 1' },
@@ -24,22 +21,22 @@ describe('Result component', () => {
         expect(screen.getByText('Player 1: 10 punten')).toBeInTheDocument();
     });
 
-
     test('renders multiple winners in case of tie', () => {
         const players = [
             {id:1, name:'Player 1'},
             {id:2, name:'Player 2'}
-        ]
+        ];
 
         const points = {
             1:10,
             2:10
-        }
+        };
 
-        render(<Result players={players} points={points}/>)
-        expect(screen.getByText('Winners')).toBeInTheDocument()
-        expect(screen.getByText('Player 1: 10 punten')).toBeInTheDocument()
-        expect(screen.getByText('Player 2: 10 punten')).toBeInTheDocument()
+        render(<Result players={players} points={points}/>);
+        expect(screen.getByText('Winners')).toBeInTheDocument();
+        expect(screen.getByText('Player 1: 10 punten')).toBeInTheDocument();
+        expect(screen.getByText('Player 2: 10 punten')).toBeInTheDocument();
+    });
 
-    })
-})
+
+});
