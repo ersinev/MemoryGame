2b2ec9e984ec1202547714c5f24a234cc16d8947
ersinev <ersinevci@hotmail.com>
2024-05-08 11:23:18 +0200

    Player Records Page updated

--- src/components/PlayerRecord.js
+++ src/components/PlayerRecord.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Row, Col } from 'react-bootstrap';
+import { MdDeleteForever } from "react-icons/md";
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,15 +9,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useEffect, useState } from 'react';
-import { Container, Button } from 'react-bootstrap';
-import { MdDeleteForever } from "react-icons/md";
 
 //"http://localhost:5000"
-//"https://memorygame-we7d.onrender.com"
-const url = "https://memorygame-we7d.onrender.com"
+  //"https://memorygame-we7d.onrender.com"
+const url = "https://memorygame-we7d.onrender.com";
 
-
+// Stil özelliklerini tanımla
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: "green",
@@ -54,29 +53,36 @@ const CustomTable = styled(Table)({
 });
 
 const CustomTableContainer = styled(TableContainer)({
+  maxHeight: 'calc(90vh)', // Adjust the height as needed
+  overflowY: 'auto',
   maxWidth: 'fit-content'
 });
 
 function PlayerRecord() {
   const [records, setRecords] = useState([]);
+  const [totalElapsedTime, setTotalElapsedTime] = useState('');
 
-  // Function to fetch player records
+  // Fonksiyonlar
   const fetchPlayerRecords = async () => {
     try {
       const response = await fetch(`${url}/player/records`);
       const data = await response.json();
       setRecords(data);
+
+      // Toplam oynama süresini hesapla
+      const totalSeconds = calculateTotalElapsedTime(data);
+      const formattedTotalTime = formatTotalTime(totalSeconds);
+      setTotalElapsedTime(formattedTotalTime);
     } catch (error) {
       console.error("Error fetching player records:", error);
     }
   };
-
   useEffect(() => {
     fetchPlayerRecords();
+    // eslint-disable-next-line
   }, []);
 
   const totalPlayers = records.length;
-  
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -84,63 +90,101 @@ function PlayerRecord() {
     return formattedDate;
   };
 
-  // Function to handle deletion of a player record
   const handleDelete = async (id) => {
     try {
       await fetch(`${url}/player/${id}`, {
         method: 'DELETE'
       });
-      // After successful deletion, fetch updated records
       fetchPlayerRecords();
     } catch (error) {
       console.error("Error deleting player record:", error);
     }
   };
 
-  // Function to handle deletion of all player records
   const handleDeleteAll = async () => {
     try {
       await fetch(`${url}/player/delete-all`, {
         method: 'DELETE'
       });
-      // After successful deletion, fetch updated records
       fetchPlayerRecords();
     } catch (error) {
       console.error("Error deleting all player records:", error);
     }
   };
 
+  function calculateTotalElapsedTime(records) {
+    let totalMilliseconds = 0;
+
+    records.forEach(record => {
+      const exitTime = new Date(record.exitTime);
+      const startTime = new Date(record.startTime);
+      const elapsedTimeInMillis = exitTime - startTime;
+      totalMilliseconds += elapsedTimeInMillis;
+    });
+
+    const totalSeconds = Math.floor(totalMilliseconds / 1000);
+
+    return totalSeconds;
+  }
+
+  function formatTotalTime(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600);
+    const remainingSeconds = totalSeconds % 3600;
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = remainingSeconds % 60;
+
+    let formattedTime = '';
+
+    if (hours > 0) {
+      formattedTime += `${hours}h `;
+    }
+    if (minutes > 0 || hours > 0) {
+      formattedTime += `${minutes}m `;
+    }
+    formattedTime += `${seconds}s`;
+
+    return formattedTime;
+  }
+
   return (
     <Container>
-
-      <p style={{color:"white", marginBottom: "5px", fontSize: "24px" }}>All-Time Players: {totalPlayers}</p>
-      <Button variant="danger" onClick={handleDeleteAll} style={{ marginBottom: '10px', padding: "5px" }}>Delete All</Button> {/* Button to delete all records */}
-      <CustomTableContainer component={Paper}>
-        <CustomTable>
-          <CustomTableHead>
-            <TableRow>
-              <StyledTableCell>Name</StyledTableCell>
-              <StyledTableCell align='center'>Date</StyledTableCell>
-              <StyledTableCell align='center'>Time</StyledTableCell>
-              <StyledTableCell align='center'>Action</StyledTableCell>
-            </TableRow>
-          </CustomTableHead>
-          <TableBody>
-            {records.slice().reverse().map((record) => (
-              <StyledTableRow key={record._id}>
-                <StyledTableCell component="th" scope="row">
-                  {record.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">{formatDate(record.startTime)}</StyledTableCell>
-                <StyledTableCell align="right">{record.elapsedTime}</StyledTableCell>
-                <StyledTableCell align="right">
-                  <Button variant="danger" onClick={() => handleDelete(record._id)}><MdDeleteForever /></Button>
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </CustomTable>
-      </CustomTableContainer>
+      <Row>
+        <Col className='mt-3'>
+          <CustomTableContainer component={Paper}>
+            <CustomTable>
+              <CustomTableHead>
+                <TableRow>
+                  <StyledTableCell>Name</StyledTableCell>
+                  <StyledTableCell align='center'>Date</StyledTableCell>
+                  <StyledTableCell align='center'>Time</StyledTableCell>
+                  <StyledTableCell align='center'>Action</StyledTableCell>
+                </TableRow>
+              </CustomTableHead>
+              <TableBody>
+                {records.slice().reverse().map((record) => (
+                  <StyledTableRow key={record._id}>
+                    <StyledTableCell component="th" scope="row">
+                      {record.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{formatDate(record.startTime)}</StyledTableCell>
+                    <StyledTableCell align="right">{record.elapsedTime}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      <Button variant="danger" onClick={() => handleDelete(record._id)}><MdDeleteForever /></Button>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </CustomTable>
+          </CustomTableContainer>
+        </Col>
+        <Col className="d-flex justify-content-center align-items-center">
+          <div style={{ marginBottom: "20px" }}>
+            <p style={{ color: "white", fontSize: "24px", border: "5px solid green", padding: "10px", borderRadius: "10px", backgroundColor: "rgba(0, 128, 0, 0.5)" }}>All-Time Players: {totalPlayers}</p>
+            <p style={{ color: "white", fontSize: "24px", border: "5px solid green", backgroundColor: "rgba(0, 128, 0, 0.5)", padding: "10px", borderRadius: "10px" }}>Total Elapsed Time: {totalElapsedTime}</p>
+            <Button variant="danger" onClick={handleDeleteAll} style={{ marginBottom: '10px', padding: "5px" }}>Delete All</Button>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
